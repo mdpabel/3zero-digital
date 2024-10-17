c95ff19219e5926396d5c5d46f80a51e3e2ba835
MD Pabel <mdpabel385@gmail.com>
2024-10-17 20:07:11 +0600

    feat: updated hero image

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -1,6 +1,10 @@
+import Image from 'next/image';
+import heroDarkGif from '@/../public/images/hero/hero-dark.gif';
+import heroLightGif from '@/../public/images/hero/hero-light.png';
+
 const Hero = () => {
   return (
-    <div className='relative bg-white dark:bg-[#030712] px-10 md:px-20 py-10 md:pt-20 '>
+    <div className='relative bg-white dark:bg-[#030712] px-10 md:px-20 py-10 md:pt-10'>
       <div className='mx-auto w-full max-w-6xl container'>
         <div className='gap-8 grid grid-cols-1 md:grid-cols-5'>
           <div className='flex flex-col justify-center col-span-3'>
@@ -30,126 +34,17 @@ const Hero = () => {
             </div>
           </div>
 
-          {/* Right Side - Custom SVG Illustration (spans 2 columns) */}
           <div className='relative flex justify-center items-center col-span-2'>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              viewBox='-30 -30 850 850' // Adjusted viewBox to provide more space
-              className='w-3/4 md:w-4/5 h-auto animate-pulse-slow'
-              fill='none'>
-              <g>
-                {/* New Outer Circle */}
-                <circle
-                  cx='400'
-                  cy='400'
-                  r='400'
-                  stroke='#614385'
-                  strokeWidth='12'
-                />
-
-                {/* Existing Outer Circle */}
-                <circle
-                  cx='400'
-                  cy='400'
-                  r='340'
-                  stroke='#614385'
-                  strokeWidth='10'
-                />
-
-                {/* Inner Circle */}
-                <circle
-                  cx='400'
-                  cy='400'
-                  r='260'
-                  stroke='#614385'
-                  strokeWidth='6'
-                />
-
-                {/* 3 Zero Elements */}
-                <text
-                  x='400'
-                  y='270'
-                  textAnchor='middle'
-                  fontSize='60'
-                  fill='#614385'
-                  fontWeight='bold'>
-                  0
-                </text>
-                <text
-                  x='400'
-                  y='330'
-                  textAnchor='middle'
-                  fontSize='24'
-                  fill='#614385'>
-                  Vulnerabilities
-                </text>
-
-                <text
-                  x='400'
-                  y='400'
-                  textAnchor='middle'
-                  fontSize='60'
-                  fill='#614385'
-                  fontWeight='bold'>
-                  0
-                </text>
-                <text
-                  x='400'
-                  y='460'
-                  textAnchor='middle'
-                  fontSize='24'
-                  fill='#614385'>
-                  Downtime
-                </text>
-
-                <text
-                  x='400'
-                  y='530'
-                  textAnchor='middle'
-                  fontSize='60'
-                  fill='#614385'
-                  fontWeight='bold'>
-                  0
-                </text>
-                <text
-                  x='400'
-                  y='590'
-                  textAnchor='middle'
-                  fontSize='24'
-                  fill='#614385'>
-                  Errors
-                </text>
-
-                {/* Connection lines between circles */}
-                <line
-                  x1='400'
-                  y1='330'
-                  x2='400'
-                  y2='270'
-                  stroke='#614385'
-                  strokeWidth='3'
-                />
-                <line
-                  x1='400'
-                  y1='460'
-                  x2='400'
-                  y2='400'
-                  stroke='#614385'
-                  strokeWidth='3'
-                />
-                <line
-                  x1='400'
-                  y1='590'
-                  x2='400'
-                  y2='530'
-                  stroke='#614385'
-                  strokeWidth='3'
-                />
-              </g>
-            </svg>
-
-            {/* Decorative Glow Effect */}
-            <div className='dark:block absolute inset-0 hidden bg-gradient-to-br from-[#614385] to-[#516395] opacity-50 blur-3xl rounded-full md:w-96 h-96 animate-spin-slow'></div>
+            <Image
+              src={heroDarkGif}
+              alt='3zero digital'
+              className='dark:block hidden'
+            />
+            <Image
+              src={heroLightGif}
+              alt='3zero digital'
+              className='block dark:hidden'
+            />
           </div>
         </div>
       </div>
