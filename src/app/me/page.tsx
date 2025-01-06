c73f1cb797dae573ddae53db31d13980f3dd75d8
MD Pabel <mdpabel385@gmail.com>
2025-01-07 02:40:54 +0600

    fix: me page error

--- src/app/me/page.tsx
+++ src/app/me/page.tsx
@@ -1,31 +1,7 @@
-import { Suspense } from 'react';
-import WelcomeMessage from './_components/welcome-message';
-import WelcomeMessageSkeleton from './_components/welcome-message-skeleton';
-import UnPaidOrders from './_components/unpaid-orders';
-import UnPaidOrdersSkeleton from './_components/unpaid-orders-skeleton';
-import OverView from './_components/overview';
-import OverViewSkeleton from './_components/overview-skeleton';
-import QuickActions from './_components/quick-actions';
+import React from 'react';
 
-const Dashboard = async () => {
-  return (
-    <div className='space-y-4'>
-      <Suspense fallback={<WelcomeMessageSkeleton />}>
-        <WelcomeMessage />
-      </Suspense>
-
-      <Suspense fallback={<OverViewSkeleton />}>
-        <OverView />
-      </Suspense>
-
-      <div className='gap-8 grid grid-cols-1 lg:grid-cols-2 mt-10'>
-        <Suspense fallback={<UnPaidOrdersSkeleton />}>
-          {/* <UnPaidOrders /> */}
-        </Suspense>
-        <QuickActions />
-      </div>
-    </div>
-  );
+const page = () => {
+  return <div>page</div>;
 };
 
-export default Dashboard;
+export default page;
