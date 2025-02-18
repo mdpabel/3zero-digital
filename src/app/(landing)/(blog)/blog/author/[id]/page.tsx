418132bb87cf88b2933ad9bf8d405d01029b0a16
MD Pabel <mdpabel385@gmail.com>
2025-02-18 13:10:01 +0600

    update: post card

--- src/app/(landing)/(blog)/blog/author/[id]/page.tsx
+++ src/app/(landing)/(blog)/blog/author/[id]/page.tsx
@@ -4,7 +4,7 @@ import Pagination from '@/components/blog/pagination';
 import SearchForm from '@/components/blog/search-form';
 import { getPosts } from '@/lib/wordpress/fetch-posts';
 import React, { Suspense } from 'react';
-import { WP_REST_API_Posts } from 'wp-types';
+import { WP_REST_API_Post, WP_REST_API_Posts } from 'wp-types';
 
 const POSTS_PER_PAGE = 5;
 
@@ -21,9 +21,17 @@ const page = async ({ params }: { params: Promise<{ id: string }> }) => {
 
   const totalPages = Math.ceil(total / POSTS_PER_PAGE);
 
+  const post = (posts as WP_REST_API_Posts)[0] as WP_REST_API_Post;
+  const author =
+    ((post as WP_REST_API_Post).yoast_head_json as any)?.author ||
+    '3Zero digital editorial';
+
   return (
     <div>
-      <SearchForm />
+      <div className='flex md:flex-row flex-col justify-between items-center'>
+        <SearchForm />
+        <h3>Posts By: {author}</h3>
+      </div>
       <BlogList style={2} posts={posts as WP_REST_API_Posts} />
       <Suspense>
         <Pagination totalPages={totalPages} />
