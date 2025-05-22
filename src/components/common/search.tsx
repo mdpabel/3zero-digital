27649e5de5079158dff79929576e3e49c93f9081
MD Pabel <mdpabel385@gmail.com>
2025-05-23 02:44:24 +0600

    fix: slug

--- src/components/common/search.tsx
+++ src/components/common/search.tsx
@@ -104,8 +104,8 @@ const SearchForm = () => {
               <div className='space-y-0.5'>
                 {services.map((item) => (
                   <Link
-                    href={item.id}
-                    key={item.slug}
+                    href={item.slug}
+                    key={item.id}
                     className={cn(
                       'relative w-full hover:bg-zinc-500 flex cursor-default gap-2 select-none items-center rounded-sm px-2 py-3 text-sm outline-none data-[disabled=true]:pointer-events-none data-[selected=true]:bg-accent data-[selected=true]:text-accent-foreground data-[disabled=true]:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
                     )}>
